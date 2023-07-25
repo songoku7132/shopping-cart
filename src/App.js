353b2e733f327908b4ba8301a800d37db2d65091
songoku7132 <uladimirsauchanka@gmail.com>
2023-07-25 11:31:19 +0300

    create shopping cart

--- src/App.js
+++ src/App.js
@@ -2,24 +2,42 @@ import { Link, Route, Routes } from 'react-router-dom'
 import  Home  from './pages/Home'
 import  Shop  from './pages/Shop'
 import './styles/App.css'
+import React, { useState, useEffect } from 'react';
 
-function App() {
+const App = () => {
+
+  const [count, setCount] = useState(0);
+
+  const addCount = (i) => {
+    const el = document.getElementById(`input${i}`);
+    let num = parseInt(el.value);
+    setCount(prevCount => prevCount + num)
+  }
+
   return (
     <>
-      <nav className='navigation'>
+      <div className='footer'>
+        <nav className='navigation'>
             <div className='navBtn'>
-            <Link className='navLink' to='/'>Home</Link>
+              <Link className='navLink' to='/'>Home</Link>
             </div>
             <div className='navBtn'>
-            <Link className='navLink' to='/shop'>Shop</Link>
+              <Link className='navLink' to='/shop'>Shop</Link>
             </div>
-      </nav>
+        </nav>
+        <div className='cart'>
+            <p className='counterTitle'>Cart {count}</p>
+        </div>
+      </div>
       <Routes>
         <Route path='/' element={<Home />} />
-        <Route path='/shop' element={<Shop />} />
+        <Route path='/shop' element={<Shop 
+        onClick={addCount}
+        />} />
       </Routes>  
     </>
   );
 }
 
 export default App;
+
--- src/pages/Shop.js
+++ src/pages/Shop.js
@@ -2,50 +2,40 @@ import '../styles/Shop.css'
 import img from '../img/homePage.jpg'
 import React, { useState, useEffect } from 'react';
 
-const Shop = () => {
-
-    const [count, setCount] = useState(0);
-
-    const addCount = (i) => {
-        const el = document.getElementById(`input${i}`)
-        let num = parseInt(el.value)
-        setCount(prevCount => prevCount + num)
-    }
-
-    const print = () => {
-        const i = document.getElementById('input1')
-        console.log(i.value)
-    }
-
+const Shop = (props) => {
     return (
         <div className="shopPage">
-          <p className='shopTitle'>Shop {count}</p>
+          <p className='shopTitle'>Shop</p>
           <div className='goodsBox'>
             <div className='product'>
                 <img src={img} />
                 <div className='addProduct'>
-                    <button onClick={() => addCount(1)}>Add</button>
+                    {/* <button onClick={() => addCount(1)}>Add</button> */}
+                    <button onClick={() => props.onClick(1)}>Add</button>
                     <input id='input1'></input>
                 </div>
             </div>
             <div className='product'>
                 <img src={img} />
                 <div className='addProduct'>
-                    <button onClick={() => addCount(2)}>Add</button>
+                    {/* <button onClick={() => addCount(2)}>Add</button> */}
+                    <button onClick={() => props.onClick(2)}>Add</button>
                     <input id='input2'></input>
                 </div>
             </div>
             <div className='product'>
                 <img src={img} />
                 <div className='addProduct'>
-                    <button onClick={() => addCount(3)}>Add</button>
+                    {/* <button onClick={() => addCount(3)}>Add</button> */}
+                    <button onClick={() => props.onClick(3)}>Add</button>
                     <input id='input3'></input>
                 </div>
             </div>
             <div className='product'>
                 <img src={img} />
                 <div className='addProduct'>
-                    <button onClick={() => addCount(4)}>Add</button>
+                    {/* <button onClick={() => addCount(4)}>Add</button> */}
+                    <button onClick={() => props.onClick(4)}>Add</button>
                     <input id='input4'></input>
                 </div>
             </div>
@@ -55,3 +45,18 @@ const Shop = () => {
 }
 
 export default Shop;
+
+
+
+    // const [count, setCount] = useState(0);
+
+    // const addCount = (i) => {
+    //     const el = document.getElementById(`input${i}`)
+    //     let num = parseInt(el.value)
+    //     setCount(prevCount => prevCount + num)
+    // }
+
+    // const print = () => {
+    //     const i = document.getElementById('input1')
+    //     console.log(i.value)
+    // }
